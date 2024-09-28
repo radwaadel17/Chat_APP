5495593a247ae7defed6f8a16940a1c240314e64
radwaadel17 <radwaadel959@gmail.com>
2024-09-28 21:12:31 +0300

    Stream builder real time changes

--- app/lib/views/chatPage.dart
+++ app/lib/views/chatPage.dart
@@ -5,15 +5,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class ChatPage extends StatefulWidget {
+class ChatPage extends StatelessWidget {
   ChatPage({super.key});
    static String id = 'ChatPage';
 
-  @override
-  State<ChatPage> createState() => _ChatPageState();
-}
-
-class _ChatPageState extends State<ChatPage> {
    //FirebaseFirestore firestore = FirebaseFirestore.instance;
    CollectionReference Messages = FirebaseFirestore.instance.collection('Messages');
 
@@ -21,77 +16,80 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<QuerySnapshot>(
-     future: Messages.get() , 
-     builder: (context , snapshot){
-      if(snapshot.hasData){
-        List<Message> ListOfMessages = [] ;
-        for(int i = 0 ; i < snapshot.data!.docs.length ; i++)
-        {
-          ListOfMessages.add(Message.fromJson(snapshot.data!.docs[i]));
-        }
-        return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: KColor,
-        leading: const Icon(
-         Icons.arrow_back,
-         color: Colors.white,
-        ),
-        title: Padding(
-          padding:  EdgeInsets.symmetric(horizontal: 80.w),
-          child: SizedBox(
-            height: 60.h,
-            child: const Row(
-              children: [
-                  Image(image: AssetImage('assets/images/Stuck at Home - Mailing List.png')),
-              ],
+    return StreamBuilder<Object>(
+      stream: Messages.snapshots(),
+      builder: (context, snapshot) {
+        return FutureBuilder<QuerySnapshot>(
+         future: Messages.get() , 
+         builder: (context , snapshot){
+          if(snapshot.hasData){
+            List<Message> ListOfMessages = [] ;
+            for(int i = 0 ; i < snapshot.data!.docs.length ; i++)
+            {
+              ListOfMessages.add(Message.fromJson(snapshot.data!.docs[i]));
+            }
+            return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: KColor,
+            leading: const Icon(
+             Icons.arrow_back,
+             color: Colors.white,
+            ),
+            title: Padding(
+              padding:  EdgeInsets.symmetric(horizontal: 80.w),
+              child: SizedBox(
+                height: 60.h,
+                child: const Row(
+                  children: [
+                      Image(image: AssetImage('assets/images/Stuck at Home - Mailing List.png')),
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: ListOfMessages.length,
-              itemBuilder: (context , index) { 
-              return chatBubble(txt: ListOfMessages[index].msg,);
-            }),
-          ),
-           Padding(
-             padding: const EdgeInsets.all(8.0),
-             child: TextField(
-              controller: controller,
-              onSubmitted: (data){
-               Messages.add({
-                  'msg':data,
-               });
-               controller.clear();
-               setState(() {
-                 
-               });
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: KColor,
-                    ),
-                    borderRadius: BorderRadius.circular(10.r),
-                ),
-                suffixIcon: IconButton(onPressed: (){}, icon:  const Icon(Icons.send),
-                  color: KColor,)
+          body: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: ListOfMessages.length,
+                  itemBuilder: (context , index) { 
+                  return chatBubble(txt: ListOfMessages[index].msg,);
+                }),
               ),
-           ),
-           ),
-        ],
-      )
+               Padding(
+                 padding: const EdgeInsets.all(8.0),
+                 child: TextField(
+                  controller: controller,
+                  onSubmitted: (data){
+                   Messages.add({
+                      'msg':data,
+                   });
+                   controller.clear();
+                
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: KColor,
+                        ),
+                        borderRadius: BorderRadius.circular(10.r),
+                    ),
+                    suffixIcon: IconButton(onPressed: (){}, icon:  const Icon(Icons.send),
+                      color: KColor,)
+                  ),
+               ),
+               ),
+            ],
+          )
+        );
+          }
+          else{
+            return Text('Loading...');
+          }
+         });
+      }
     );
-      }
-      else{
-        return Text('Loading...');
-      }
-     });
   }
 }
 
